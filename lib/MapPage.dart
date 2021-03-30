420abdbfe95c6c0afb09ac757d418965aa396262
Thanut Pheeraphan <thanut@wongnai.com>
2021-03-30 10:44:39 +0700

    Google Map with Marker but not yet clickable

--- lib/MapPage.dart
+++ lib/MapPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   @override
@@ -6,15 +7,31 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  Set<Marker> _markers = {};
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      _markers.add(Marker(
+          markerId: MarkerId("id-1"), position: LatLng(13.756331, 100.501762)));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("MapPage"),
-      ),
-      body: new Center(
-        child: new Text("This is Map Page"),
-      ),
-    );
+    return Scaffold(
+        appBar: new AppBar(
+          title: new Text("GoogleMaps"),
+        ),
+        body: GoogleMap(
+            onMapCreated: _onMapCreated,
+            markers: _markers,
+            initialCameraPosition: CameraPosition(
+              target: LatLng(13.756331, 100.501762),
+              zoom: 15,
+            )));
   }
 }
+
+//https://www.youtube.com/watch?v=gTHKFRRSPss&t=537s infowindow normal
+//https://console.cloud.google.com/apis/credentials?folder=&organizationId=&project=just-be-there-307718&supportedpurview=project generate API KEY
+//https://www.youtube.com/watch?v=0048xewPoxo infowindow on click
