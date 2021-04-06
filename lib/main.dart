eb7f7a146341dad383ded69957cc441d71e69676
Thanut Pheeraphan <thanut@wongnai.com>
2021-04-06 18:32:21 +0700

    android compatible

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:jbt/SignUp/SignUpScreen.dart';
+
 import 'LogIn/LogInScreen.dart';
 import 'NewEvent/NewEventScreen.dart';
 import 'dummy.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
