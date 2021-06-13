14f77c6f2fdb66917b3d0eede5474ce0d3b02f87
Thanut Pheeraphan <thanut@wongnai.com>
2021-06-13 17:49:20 +0700

    Removed DEBUG header

--- lib/Overview/OverviewScreen.dart
+++ lib/Overview/OverviewScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jbt/Overview/slide.dart';
+
 import '../helper.dart';
-import 'dart:io';
 
 class OverviewScreen extends StatefulWidget {
   @override
@@ -35,74 +35,77 @@ class OverviewScreenState extends State<OverviewScreen> {
   @override
   Widget build(BuildContext context) {
     Size deviceSize = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(
-          color: appColor.lightGray,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(
+            color: appColor.lightGray,
+          ),
+          centerTitle: true,
+          backgroundColor: appColor.green,
+          title: Text(
+            "Overview",
+            style: appTextStyle.bold18Gray,
+          ),
         ),
-        centerTitle: true,
-        backgroundColor: appColor.green,
-        title: Text(
-          "Overview",
-          style: appTextStyle.bold18Gray,
+        backgroundColor: Colors.white,
+        body: PageView.builder(
+          controller: _pageController,
+          onPageChanged: (val) {
+            setState(() {
+              current = val;
+            });
+          },
+          itemBuilder: (context, index) {
+            return Tile(
+              image: slides[index].image,
+              title: slides[index].title,
+              detail: slides[index].detail,
+            );
+          },
+          itemCount: slides.length,
         ),
-      ),
-      backgroundColor: Colors.white,
-      body: PageView.builder(
-        controller: _pageController,
-        onPageChanged: (val) {
-          setState(() {
-            current = val;
-          });
-        },
-        itemBuilder: (context, index) {
-          return Tile(
-            image: slides[index].image,
-            title: slides[index].title,
-            detail: slides[index].detail,
-          );
-        },
-        itemCount: slides.length,
-      ),
-      bottomSheet: Container(
-        height: deviceSize.height * 0.05,
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            GestureDetector(
-              onTap: () {
-                _pageController.animateToPage(
-                  current - 1,
-                  duration: Duration(milliseconds: 400),
-                  curve: Curves.linear,
-                );
-              },
-              child: Text(
-                "PREV",
-                style: appTextStyle.regular13Green,
+        bottomSheet: Container(
+          height: deviceSize.height * 0.05,
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  _pageController.animateToPage(
+                    current - 1,
+                    duration: Duration(milliseconds: 400),
+                    curve: Curves.linear,
+                  );
+                },
+                child: Text(
+                  "PREV",
+                  style: appTextStyle.regular13Green,
+                ),
               ),
-            ),
-            Row(
-              children: <Widget>[
-                for (int i = 0; i < slides.length; i++)
-                  current == i ? indicator(true) : indicator(false)
-              ],
-            ),
-            GestureDetector(
-              onTap: () {
-                _pageController.animateToPage(
-                  current + 1,
-                  duration: Duration(milliseconds: 400),
-                  curve: Curves.linear,
-                );
-              },
-              child: Text(
-                "NEXT",
-                style: appTextStyle.regular13Green,
+              Row(
+                children: <Widget>[
+                  for (int i = 0; i < slides.length; i++)
+                    current == i ? indicator(true) : indicator(false)
+                ],
               ),
-            ),
-          ],
+              GestureDetector(
+                onTap: () {
+                  _pageController.animateToPage(
+                    current + 1,
+                    duration: Duration(milliseconds: 400),
+                    curve: Curves.linear,
+                  );
+                },
+                child: Text(
+                  "NEXT",
+                  style: appTextStyle.regular13Green,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
